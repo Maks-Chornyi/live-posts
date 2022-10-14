1f590858e5133cca1b47ca6dcae8b01ffe3b2b13
Maks-Chornyi <--replace-all>
2022-10-14 20:51:08 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './auth/auth.component';
+import { PostComponent } from './post/post.component';
+import { PostAddEditComponent } from './post-add-edit/post-add-edit.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthComponent,
+    PostComponent,
+    PostAddEditComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/post-add-edit/post-add-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-add-edit',
+  templateUrl: './post-add-edit.component.html',
+  styleUrls: ['./post-add-edit.component.css']
+})
+export class PostAddEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
