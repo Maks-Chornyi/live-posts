fb778819499b0ac2f5e436364f2e2f563347b0b2
Maks-Chornyi <--replace-all>
2022-10-22 17:35:53 +0300

    Added like post

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -9,21 +9,24 @@ export class PostService {
       'Nature, in the broadest sense, is the physical world or universe. "Nature" can refer to the phenomena of the physical world, and also to life in general. The study of nature is a large, if not the only, part of science. Although humans are part of nature, human activity is often understood as a separate category from other natural phenomena.',
       'https://global-uploads.webflow.com/5df3d56e20b6d37f8de5e660/5e078aafed2a27217e6f09a2_memory-in-nature.jpg',
       'Maks',
-      new Date()
+      new Date(),
+      3,
     ),
     new Post(
       'Space',
       'Space is the boundless three-dimensional extent in which objects and events have relative position and direction.',
       'https://a.pinatafarm.com/960x640/00a5009d4e/james-webb-space-telescope.jpeg',
       'Maks',
-      new Date()
+      new Date(),
+      2,
     ),
     new Post(
       'Jappware',
       'Jappware provides high-quality IT Consulting & Software Development. Offers full-cycle development process from idea to release using top technologies and intelligent solutions. Our attention to detail and quality is unmatched in the IT industry.',
       'https://www.jappware.com/wp-content/uploads/2022/01/photo_2022-01-26-15.51.09-2.jpeg',
       'Maks',
-      new Date()
+      new Date(),
+      1,
     ),
   ];
 
@@ -48,5 +51,9 @@ export class PostService {
 
   public editPost(index: number, post: Post): void {
     this.listOfPosts[index] = post;
+  }
+
+  public likePost(index: number) {
+      this.listOfPosts[index].numberOfLikes += 1;
   }
 }
