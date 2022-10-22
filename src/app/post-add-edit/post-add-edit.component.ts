437eec80c22faaea73316d41dbacef998c8835db
Maks Chornyi <makschornyi.com@gmail.com>
2022-10-22 15:56:58 +0300

    Added post editing functionality (#6)
    Co-authored-by: Maks-Chornyi <--replace-all>

--- src/app/post-add-edit/post-add-edit.component.ts
+++ src/app/post-add-edit/post-add-edit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Post} from "../post.model";
 import {PostService} from "../post.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-post-add-edit',
@@ -13,13 +13,32 @@ export class PostAddEditComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor(private postService: PostService, private router: Router) { }
+  constructor(
+    private postService: PostService,
+    private router: Router,
+    private route: ActivatedRoute,) { }
+    private postIndex: number = 0;
+    public editMode: boolean = false;
 
   ngOnInit(): void {
+    let title: string = '';
+    let description: string = '';
+    let imagePath: string = '';
+
+    this.route.params.subscribe((params: Params) => {
+      if (params['index']) {
+        this.editMode = true;
+        this.postIndex = params['index'];
+        const post: Post = this.postService.getPostByIndex(this.postIndex);
+        title = post.title;
+        description = post.description;
+        imagePath = post.imagePath;
+      }
+    });
     this.form = new FormGroup({
-        title: new FormControl(null,[Validators.required, Validators.maxLength(10)]),
-        description: new FormControl(null, [Validators.required]),
-        imagePath: new FormControl(null, [Validators.required])
+        title: new FormControl(title,[Validators.required, Validators.maxLength(10)]),
+        description: new FormControl(description, [Validators.required]),
+        imagePath: new FormControl(imagePath, [Validators.required])
       }
     );
   }
@@ -29,10 +48,13 @@ export class PostAddEditComponent implements OnInit {
     const description = this.form.value.description;
     const imagePath = this.form.value.imagePath;
 
-    //ready with object
     const post: Post = new Post (title, description, imagePath, 'maks@maks.com', new Date());
 
-    this.postService.addPost(post);
+    if (this.editMode) {
+      this.postService.editPost(this.postIndex, post);
+    } else {
+      this.postService.addPost(post);
+    }
 
     //navigate to post list
     this.router.navigate(['/post-list']).then(r => console.log(r));
